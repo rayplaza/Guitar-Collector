60c7f6a7c9a621b83722e0826a7531e3d3ff74d4
Ray Plaza <munditoplaza@gmail.com>
2019-12-14 23:01:24 -0800

    completed first set

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('guitars/', views.guitars_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def guitars_index(request):
+    return render(request, 'guitars/index.html', { 'guitars': guitars })
+
