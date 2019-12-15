4031c5f8a85099d88d4bda849d1695c35d6c7a91
Ray Plaza <munditoplaza@gmail.com>
2019-12-14 22:52:59 -0800

    created about and view all templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
