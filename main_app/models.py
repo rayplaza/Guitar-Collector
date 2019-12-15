e7c26e716b8d022b7a528d7e31f84cac5df490fe
Ray Plaza <munditoplaza@gmail.com>
2019-12-14 23:26:47 -0800

    migrated model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Guitar(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    wood = models.CharField(max_length=100)
+    pickup = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+
+
+    
+    
