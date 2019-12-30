73b7df724ea2d760d3cc975a21956d0373d66b22
Ray Plaza <munditoplaza@gmail.com>
2019-12-29 21:51:44 -0800

    updated models with user step 4 auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 SERVICES = (
     ('S', 'Strings'),
@@ -29,6 +31,7 @@ class Guitar(models.Model):
     wood = models.CharField(max_length=100)
     pickup = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
